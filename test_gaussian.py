a755dc335cc76f8d4fc2ff0369836139408bd394
Jason Hobbs <jason.hobbs@gmail.com>
2022-05-02 23:18:23 -0500

    Add some more test cases

--- test_gaussian.py
+++ test_gaussian.py
@@ -1,5 +1,5 @@
 import pytest
-from sympy import FiniteSet, Rational
+from sympy import FiniteSet, Rational, EmptySet
 
 from gaussian import LinearSystemOfEquations
 
@@ -8,6 +8,7 @@ from gaussian import LinearSystemOfEquations
     "system, solution",
     (
         (
+            # 1.19a
             LinearSystemOfEquations.from_string("2x +2y = 5,x -4y = 0"),
             FiniteSet(
                 (
@@ -17,11 +18,30 @@ from gaussian import LinearSystemOfEquations
             ),
         ),
         (
+            # 1.19b
             LinearSystemOfEquations.from_string("-x +y = 1, x +y = 2"),
             FiniteSet(
                 (
                     Rational("1/2"),
                     Rational("3/2"),
+                )
+            ),
+        ),
+        (
+            # 1.19d
+            LinearSystemOfEquations.from_string("-x -y = 1, -3x -3y = 2"),
+            EmptySet,
+        ),
+        (
+            # 1.19e
+            LinearSystemOfEquations.from_string(
+                "4y +z = 20, 2x -2y +z = 0, x +z = 5, x +y -z = 10"
+            ),
+            FiniteSet(
+                (
+                    5,
+                    5,
+                    0,
                 )
             ),
         ),
